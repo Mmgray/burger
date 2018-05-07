1841f6485c9619bcfb10fea8855142262c3cf83a
Misti Gray <misti.mechelle@gmail.com>
2018-05-06 22:42:57 -0500

    added database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss burger');
+INSERT INTO burgers (burger_name) VALUES ('Stuffed triple cheese aad bacon burger');
